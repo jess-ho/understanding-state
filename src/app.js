c60f78d764bb46f781fad17a67a83d9641d014c5
Jessica H <Jessica H>
2017-03-13 16:10:50 -0400

    Added div app in html

--- src/app.js
+++ src/app.js
@@ -1,1 +1,26 @@
-console.log('coffeeeeee')
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import Search from './searchbar.js'
+
+class Submitbutton extends React.Component {
+	render() {
+		return (
+			<button className="search">Search</button>
+		)
+	}
+}
+
+class App extends React.Component {
+	render() {
+		return (
+			<header>
+				<h1>Hi</h1>  
+				<p>This is a paragraph</p>
+				<Search /><Submitbutton />
+			</header>
+		)
+	}
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
