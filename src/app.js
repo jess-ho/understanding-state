4d5beb9320e1e400a7c3a86c31ea230193f6b638
Jessica H <Jessica H>
2017-03-14 11:56:41 -0400

    Understanding states

--- src/app.js
+++ src/app.js
@@ -1,13 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+class Counter extends React.Component {
+	constructor() {
+		super()
+		this.state = {
+			visitors: 0
+		}
+		this.increment = this.increment.bind(this)
+	}
+	increment() {
+		this.setState({
+			visitors: this.state.visitors + 1
+		})
+	}
+	render() {
+		return (
+			<div>
+				You are visitor number {this.state.visitors}.
+				<button onClick={this.increment}>New Visitor!</button>
+			</div>
+		)
+	}
+}
+
 class App extends React.Component {
 	render() {
 		return (
 			<header>
 				<h1>Hi</h1>  
-				<p>This is a paragraph</p>
-				<Search /><Submitbutton />
+				<Counter />
 			</header>
 		)
 	}
